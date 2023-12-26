e10a8d3e70ada5ff35bd53262743c68cad5a8f91
gigabite-hub <king46381@gmail.com>
2023-12-26 19:11:53 +0500

    Analytics added

--- functions.php
+++ functions.php
@@ -49,40 +49,91 @@ function add_new_analytics_tab($items)
     return $items;
 }
 
+function formatNumber($number) {
+    if ($number >= 1000) {
+        // Format as "4K" for thousands
+        return round($number / 1000, 1) . 'K';
+    } else {
+        // Leave unchanged if less than 1000
+        return $number;
+    }
+}
+
+function calculatePercentage($value, $total) {
+    if ($total > 0) {
+        return round(($value / $total) * 100, 1) . '%';
+    } else {
+        return 'N/A'; // Avoid division by zero
+    }
+}
+
 add_action('woocommerce_account_analytics-support_endpoint', 'analytics_tab_content');
 
 function analytics_tab_content()
 {
     echo '<h4><strong>Analytics</strong></h4>';
 
+    // Replace these variables with your actual values
+    $page_id = '234502746749024';
+    $page_access_token = 'EAAEqipn0O4cBOznj7I7QRO3FXaNZA97q4PrhxMtclyWOm8kG8fZCzyEF7ZB1I2tmu658BNbfhyJksrPD1ZCtLVqS4hMAmjscTuAlqcazMMscAP26BukMoGtaqhrZCTetYFNWitKtmt7lXt1n50AmHdNx8P4YZARmUFzAKOzI6XvQeaWbOsI63h3R70bad04QcZD';
+
+    // Get total followers
+    $followersEndpoint = "https://graph.facebook.com/v13.0/{$page_id}?fields=fan_count&access_token={$page_access_token}";
+    $followersResponse = json_decode(file_get_contents($followersEndpoint), true);
+    $totalFollowers = $followersResponse['fan_count'];
+
+    // Get total page likes
+    $pageLikesEndpoint = "https://graph.facebook.com/v13.0/{$page_id}/insights/page_fans?access_token={$page_access_token}";
+    $pageLikesResponse = json_decode(file_get_contents($pageLikesEndpoint), true);
+    $totalPageLikes = $pageLikesResponse['data'][0]['values'][0]['value'];
+
+    // Get total page views
+    $pageViewsEndpoint = "https://graph.facebook.com/v13.0/{$page_id}/insights/page_views_total?access_token={$page_access_token}";
+    $pageViewsResponse = json_decode(file_get_contents($pageViewsEndpoint), true);
+    $totalPageViews = $pageViewsResponse['data'][0]['values'][0]['value'];
+
+    // Get total page engagement
+    $pageEngagementEndpoint = "https://graph.facebook.com/v13.0/{$page_id}/insights/page_engaged_users?access_token={$page_access_token}";
+    $pageEngagementResponse = json_decode(file_get_contents($pageEngagementEndpoint), true);
+    $totalPageEngagement = $pageEngagementResponse['data'][0]['values'][0]['value'];
+		
+
+    // Display the results
+    // echo "Total Followers: " . formatNumber($totalFollowers) . PHP_EOL;
+    // echo "Total Page Likes: " . formatNumber($totalPageLikes) . PHP_EOL;
+    // echo "Total Page Views: " . formatNumber($totalPageViews) . PHP_EOL;
+    // echo "Total Page Engagement: " . formatNumber($totalPageEngagement) . PHP_EOL;
+
+    // echo "Total Followers: " . formatNumber($totalFollowers) . ' (' . calculatePercentage($totalFollowers, $totalFollowers) . ')' . PHP_EOL;
+    // echo "Total Page Likes: " . formatNumber($totalPageLikes) . ' (' . calculatePercentage($totalPageLikes, $totalFollowers) . ')' . PHP_EOL;
+    // echo "Total Page Views: " . formatNumber($totalPageViews) . ' (' . calculatePercentage($totalPageViews, $totalFollowers) . ')' . PHP_EOL;
+    // echo "Total Page Engagement: " . formatNumber($totalPageEngagement) . ' (' . calculatePercentage($totalPageEngagement, $totalFollowers) . ')' . PHP_EOL;
+
+
     // HTML structure for three cards
     echo '<div class="analytics-cards-container">
         <div class="analytics-card followers">
             <p>Followers</p>
-            <h4>34.5K</h4>
-            <p>26.84%</p>
+            <h4>'.formatNumber($totalFollowers) . PHP_EOL.'</h4>
+            <p>'.calculatePercentage($totalFollowers, $totalFollowers).'</p>
         </div>
 
         <div class="analytics-card engagement">
         <p>Engagement</p>
-        <h4>34.5K</h4>
-        <p>26.84%</p>
+        <h4>'.formatNumber($totalPageEngagement) . PHP_EOL.'</h4>
+        <p>'.calculatePercentage($totalPageEngagement, $totalFollowers).'</p>
         </div>
 
         <div class="analytics-card total-views">
         <p>Total Views</p>
-        <h4>34.5K</h4>
-        <p>26.84%</p>
+        <h4>'.formatNumber($totalPageViews) . PHP_EOL.'</h4>
+        <p>'.calculatePercentage($totalPageViews, $totalFollowers).'</p>
         </div>
 
         <div class="analytics-card total-likes">
         <p>Total Likes</p>
-        <h4>34.5K</h4>
-        <p>26.84%</p>
-        </div>
-        <div class="analytics-card profile-discovery">
-        <p><strong>Profile Discovery</strong></p>
-        <p>26.84% (From last month)</p>
+        <h4>'.formatNumber($totalPageLikes) . PHP_EOL.'</h4>
+        <p>'.calculatePercentage($totalPageLikes, $totalFollowers).'</p>
         </div>
     </div>';
 }
